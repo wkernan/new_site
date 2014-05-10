68944f853694a7cfd673c789181b536f478e2dbe
wkernan <wkernan@gmail.com>
2014-05-10 13:42:44 -0500

    controller helper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
 	protect_from_forgery with: :exception
+	before_action :home
 
 	def home
-		response = HTTParty.get('https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_balance')
+		@balance = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_balance").body)['data']['balance']
   	end
 end
