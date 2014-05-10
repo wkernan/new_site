cc2980692995ec9863038cdadaf6e4d905bd8adf
wkernan <wkernan@gmail.com>
2014-05-10 14:11:55 -0500

    showing first @amount

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
 
 	def home
 		@balance = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_balance").body)['data']['balance']
-		#@amount0 = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive").body)['data']['transactions'][0]['amount']
+		@amount0 = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive").body)['data']['transactions'][0]['amount']
 		#@amount1 = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive").body)['data']['transactions'][1]['amount']
 		#@amount2 = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive").body)['data']['transactions'][2]['amount']
 		#@amount3 = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive").body)['data']['transactions'][3]['amount']
