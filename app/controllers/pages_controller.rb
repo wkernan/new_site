6c2a61793dba55e01f731186f0b7f445594b6662
wkernan <wkernan@gmail.com>
2014-05-26 10:26:43 -0500

    change api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,10 @@ class PagesController < ApplicationController
 	def home
 		@balance = HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_balance")['data']['balance']
 		@amount0 = HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive")['data']['transactions'][0]['amount'].to_i
-		@amount1 = HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive")['data']['transactions'][1]['amount'].to_i
-		@amount2 = HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive")['data']['transactions'][2]['amount'].to_i
-		@amount3 = HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive")['data']['transactions'][3]['amount'].to_i
-		@amount4 = HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive")['data']['transactions'][4]['amount'].to_i
+		#@amount1 = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive").body)['data']['transactions'][1]['amount']
+		#@amount2 = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive").body)['data']['transactions'][2]['amount']
+		#@amount3 = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive").body)['data']['transactions'][3]['amount']
+		#@amount4 = JSON.parse(HTTParty.get("https://www.dogeapi.com/wow/v2/?api_key=1e0zh9wruemcmom3r4kml92kg&a=get_transactions&num=5&type=receive").body)['data']['transactions'][4]['amount']
 		@balance2 = HTTParty.get("https://dogechain.info/chain/Dogecoin/q/addressbalance/DHRVaPLM9c5idu3nKbKN8m182v1Kjqsmoq")
   	end
 end
